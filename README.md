870aea71be0f296afa18daf48634c6213042dfce
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-08-16 17:01:18 -0700

    kinda stuck on tabs

--- README.md
+++ README.md
@@ -29,14 +29,23 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager
 
 1. What is the DOM?
+The DOM is an object-based representation of the source HTML document.
 
 2. What is an event?
 
+An event is an interaction of a user with the site such as click, keydown, drag etc.
+
 3. What is an event listener?
+
+An event listener is put on an element so that when that specific event happens, the element will fire a callback.
 
 4. Why would we convert a NodeList into an Array?
 
+So that we could use array methods that cannot be used on a NodeList such as .reduce or .map.
+
 5. What is a component?
+
+A component is a building block for web development as they are reusable.
 
 ### Git Set up
 
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,37 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+let data = [];
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        data = response.data.topics;
+        let topicsElement = document.querySelector(".topics");
+        topicsElement.appendChild(createTopics(data));
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+function createTopics(data) {
+    
+    // console.log(data);
+    let trends = [];
+    let topics = document.createElement("div");
+    for (let i = 0; i < data.length; i++){
+        trends.push(document.createElement("div"));
+    }
+    for (let i = 0; i < trends.length; i++){
+        trends[i].textContent = data[i].textContent;
+        trends[i].classList.add("tab");
+        topics.appendChild(trends[i]);
+      }
+    return topics;
+
+    
+}
+
+// let topicsElement = document.querySelector(".topics");
+// console.log(data);
+// topicsElement.appendChild(createTopics(data));
